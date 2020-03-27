215e00c29765833a5335f14f2304891229802855
Valentin Abeille <valentin.abeille@gmail.com>
2020-03-27 18:40:07 +0100

    Modified typo in schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 2020_03_27_073622) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-
   create_table "cartons", force: :cascade do |t|
     t.string "matiere"
     t.string "annee"
